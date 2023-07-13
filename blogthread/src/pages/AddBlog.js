c472585dd670db70f3704b2f93d325c6d86f3daf
abhishek lohani <alohani1308@gmail.com>
2023-07-14 02:11:34 +0530

    blog

--- blogthread/src/pages/AddBlog.js
+++ blogthread/src/pages/AddBlog.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../component/AddBlog.css';
 
-function AddBlog() {
+function AddBlog( { handlNewblog } ) {
+
+  const [addblog,setAddblog]=useState();
+
+  const handleChange=(e)=>{
+    setAddblog(e.target.value);
+  }
+
+  console.log(addblog);
+
   return (
-    <div>AddBlogs</div>
+    <div className='add--blog'>
+      <textarea rows={10} cols={50} onChange={handleChange} value={addblog} />
+      <button className='btn'>Add Blog</button>
+    </div>
   )
 }
 
